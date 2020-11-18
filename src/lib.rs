ba88d0400e2e0c5fd223a0875f69b9a35a15cfc1
James Wang <jameswang9909@hotmail.com>
2020-11-18 16:27:07 -0600

    Create public function for reading a program

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,19 @@ pub mod instruction;
 pub mod opcode;
 pub mod operand;
 pub mod vm;
+
+use std::fs::File;
+use std::io::Read;
+
+pub fn read_program(filename: &str) -> Result<Vec<u16>, std::io::Error> {
+    let mut f = File::open(filename)?;
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)?;
+
+    // Convert the input so we're operating only on u16 values
+    Ok(buffer
+        .chunks_exact(2)
+        .into_iter()
+        .map(|b| u16::from_ne_bytes([b[0], b[1]]))
+        .collect())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::Read;
 
 use synacor::vm::Vm;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: {} <file> ", &args[0]);
@@ -13,16 +10,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let filename = &args[1];
-    let mut f = File::open(filename)?;
-    let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer)?;
-
-    // Convert the program so we're operating only on u16 values
-    let mut code: Vec<u16> = buffer
-        .chunks_exact(2)
-        .into_iter()
-        .map(|b| u16::from_ne_bytes([b[0], b[1]]))
-        .collect();
+    let mut code = synacor::read_program(filename)?;
     let mut vm = Vm::new(&mut code);
 
     if let Err(e) = vm.run() {
